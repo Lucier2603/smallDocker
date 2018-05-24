b9cd003b5f1f6694155aa860bf82b4979344c021
zhangchen20 <zhangchen20@baidu.com>
2018-05-24 16:44:12 +0800

    command

--- command/command.go
+++ command/command.go
@@ -18,21 +18,3 @@ type Command interface {
 }
 
 
-// exec 以交互方式进入某个容器
-type ExecCommand struct {
-	id int
-}
-
-
-func (this *ExecCommand) Process() {
-
-}
-
-
-// ps 查看当时正在运行的容器
-type PsCommand struct {
-}
-
-func (this *PsCommand) Process() {
-
-}
